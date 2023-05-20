aa8d524c01190fa5e51a9463b0073c201ab9d415
iam-bkpl <kapil.bhandari@apexcollege.edu.np>
2023-05-20 21:12:19 +0545

    tower of hanoi

---
+++ exam/towerOfHanoi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void towerOfHanoi(int n, char source, char destination, char auxiliary)
+{
+ if (n == 1)
+ {
+  printf("Move disk 1 from %c to %c\n", source, destination);
+  return;
+ }
+
+ towerOfHanoi(n - 1, source, auxiliary, destination);
+ printf("Move disk %d from %c to %c\n", n, source, destination);
+ towerOfHanoi(n - 1, auxiliary, destination, source);
+}
+
+int main()
+{
+ int n;
+ printf("Enter the number of disks: ");
+ scanf("%d", &n);
+
+ printf("Tower of Hanoi solution:\n");
+ towerOfHanoi(n, 'S', 'D', 'A');
+
+ return 0;
+}
